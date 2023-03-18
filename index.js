64a4c180416d214adcfa1bcee87e54735d1e5c59
Ben Robinson <ben@click-on.co.uk>
2023-03-18 19:55:56 +0000

    Finished i think

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const multer  = require('multer');
+const cors = require('cors');
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +13,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file;
+  const retVal = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  }
+  res.json(retVal);  
+});
 
 
 
